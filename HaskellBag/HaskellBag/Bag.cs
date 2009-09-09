2f08f571d7231d124a3c5c511aa8a4d434c2fcd0
Kevin Jacobson <kj@csh.rit.edu>
2009-09-08 21:29:39 -0400

    -a

---
+++ HaskellBag/HaskellBag/Bag.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System;
+
+
+
+namespace HaskellBag
+{
+	public delegate T MappingFunction<T>(T item);
+	
+	public delegate bool Predicate<T>(T item);
+	
+	public class Bag<T> : List<T>
+	{	
+		public Bag<T> Where(Predicate<T> predicate){
+			Bag<T> newBag = new Bag<T>();
+			foreach(T item in this){
+				if(predicate(item)){
+					newBag.Add(item);
+				}
+			}
+			return newBag;
+			
+		}
+		
+		public Bag<T> Map(MappingFunction<T> function){
+			Bag<T> newBag = new Bag<T>();
+			foreach(T item in this){
+				newBag.Add(function(item));
+			}
+			return newBag;
+		}
+	}
+	
+	
+}
---
+++ HaskellBag/HaskellBag/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HaskellBag
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
